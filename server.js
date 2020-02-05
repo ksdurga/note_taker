c49b33dc0eb6c8b1b43efb2f0316f3081fa7084f
Katie Durga <katie.durga@gmail.com>
2020-02-04 23:00:29 -0500

    added things from gitlab

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => {
